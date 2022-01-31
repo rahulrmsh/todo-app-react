5291b6ce862a8d2ad6c173fe4d7760ae3e78df48
rahulrmsh <rahulramesh13.rr@gmail.com>
2022-01-31 10:56:41 +0530

    todo components added

---
+++ src/components/FilterButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FilterButton (props) {
+  return (
+
+    <button type='button' className='btn toggle-btn' aria-pressed='true'>
+      <span className='visually-hidden'>Show </span>
+      <span>{props.title}</span>
+      <span className='visually-hidden'> tasks</span>
+    </button>
+
+  )
+}
